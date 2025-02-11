fed397a1c816bb277e56787904bd08028fd9d943
Oscar <trabajos1379@gmail.com>
2025-02-11 20:22:17 +0100

    creado carpeta, archivo y ruta inicial para create-account

--- src/core/router/router.component.tsx
+++ src/core/router/router.component.tsx
@@ -1,12 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { appRoutes } from "@/core/router";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { appRoutes } from '@/core/router';
 import {
   LoginPage,
   AccountListPage,
-  AccountPage,
+  /* AccountPage, */
   MovementListPage,
   TransferPage,
-} from "@/pages";
+  CreateAccountPage,
+} from '@/pages';
 
 export const Router = () => {
   return (
@@ -14,8 +15,8 @@ export const Router = () => {
       <Routes>
         <Route path={appRoutes.root} element={<LoginPage />} />
         <Route path={appRoutes.accountList} element={<AccountListPage />} />
-        <Route path={appRoutes.createAccount} element={<AccountPage />} />
-        <Route path={appRoutes.editAccount} element={<AccountPage />} />
+        <Route path={appRoutes.createAccount} element={<CreateAccountPage />} />
+        <Route path={appRoutes.editAccount} element={<CreateAccountPage />} />
         <Route path={appRoutes.movements} element={<MovementListPage />} />
         <Route path={appRoutes.transfer} element={<TransferPage />} />
         <Route
---
+++ src/pages/create-account/create-account.list.page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const CreateAccountPage: React.FC = () => {
+  return <h1>Create account</h1>;
+};
